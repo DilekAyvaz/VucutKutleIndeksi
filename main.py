2d20e09fec39cc9999b04cc05cd91a31f5e6f4d3
DİLEK <dilekayvaz0@gmail.com>
2026-02-20 21:47:48 +0300

    Enhance BMI calculator functionality and UI. Added a new function to categorize BMI results and updated label fonts for better readability.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import tkinter 
+from tkinter import font
 
 window = tkinter.Tk()
 window.title("Vücut Kitle Endeksi Hesaplama")
 window.config(padx=20, pady=20)
+
+def vki_hesapla(vki):
+     
+    result_string=f"Your VKİ is:{vki}.you are "
+    if vki<18.5:
+            return "Zayıf","yellow"
+    elif vki<25:
+            return "Normal","red"
+    elif vki<30:
+            return "Fazla Kilolu","blue"
+    else:
+            return "Obez","pink"
+
 
 def hesapla_button_clicked():
     kilo=kilo_entry.get()
@@ -13,41 +27,32 @@ def hesapla_button_clicked():
     else:
         try:
             vki=float(kilo)/(float(boy)/100)**2
-            result = f"Vücut Kitle Endeksi: {round(vki, 2)}.you are"
+            kategori,renk =vki_hesapla(vki)
+            result_label.config(text=f"Vücut Kitle Endeksi: {vki:.2f}→ {kategori}",fg=renk)
+
         except:
-            result_label.config(text="Lütfen geçerli bir kilo ve boy bilgisi giriniz")
+            result_label.config(text="Lütfen geçerli bir kilo ve boy bilgisi giriniz",fg="white")
             
-    def vki_hesapla(vki):
-     
-        if vki<18.5:
-            return "Zayıf"
-        elif vki<25:
-            return "Normal"
-        elif vki<30:
-            return "Fazla Kilolu"
-        else:
-            return "Obez"
-                
-    result_label.config(text=vki_hesapla(vki))
     
 #ui
-kilo_label = tkinter.Label(text="Kilo (kg):" , font=("fredoka one", 16, ))
+kilo_label = tkinter.Label(text="Kilo (kg):" , font=("academy engraved let", 22, ))
 kilo_label.pack()
-
 kilo_entry = tkinter.Entry()
 kilo_entry.pack()
 
-boy_label = tkinter.Label(text="Boy (cm):")
+boy_label = tkinter.Label(text="Boy (cm):" , font=("academy engraved let",22))
 boy_label.pack()
-
 boy_entry = tkinter.Entry()
 boy_entry.pack()
 
-hesapla_button = tkinter.Button(text="Hesapla" ,command=hesapla_button_clicked)
+hesapla_button = tkinter.Button(text="Hesapla" ,font=("academy engraved let", 22, ),command=hesapla_button_clicked)
 hesapla_button.pack()
 
-result_label = tkinter.Label(text="")
+result_label = tkinter.Label(text="",font=("academy engraved let",19))
 result_label.pack()
 
-
+                
 window.mainloop()
+
+
+
